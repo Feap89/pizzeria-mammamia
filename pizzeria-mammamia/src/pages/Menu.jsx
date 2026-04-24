65aba830f67698a9e8f677f37a578daa88846dc6
Fabricio EAP <feapjiap@gmail.com>
2026-04-24 16:10:25 -0400

    Renombra imágenes sin espacios y actualiza Menu.jsx

--- pizzeria-mammamia/src/pages/Menu.jsx
+++ pizzeria-mammamia/src/pages/Menu.jsx
@@ -1,11 +1,11 @@
 import PizzaCard from "../components/PizzaCard";
-import margarita from "../assets/Pizza 01.png";
-import pepperoni from "../assets/Pizza 2.png";
-import vegetariana from "../assets/Pizza 3.png";
+import margarita from "../assets/pizza01.png";
+import pepperoni from "../assets/pizza02.png";
+import vegetariana from "../assets/pizza03.png";
 
 function Menu() {
   return (
-    <div>
+    <div className="menu">
       <h2>Menú de Pizzas</h2>
       <PizzaCard nombre="Margarita" precio={8000} imagen={margarita} />
       <PizzaCard nombre="Pepperoni" precio={9500} imagen={pepperoni} />
@@ -15,10 +15,3 @@ function Menu() {
 }
 
 export default Menu;
-
-<div className="menu">
-  <PizzaCard nombre="Margarita" precio={8000} imagen={margarita} />
-  <PizzaCard nombre="Pepperoni" precio={9500} imagen={pepperoni} />
-  <PizzaCard nombre="Vegetariana" precio={9000} imagen={vegetariana} />
-</div>
-
